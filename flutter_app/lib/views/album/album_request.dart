0f2dcd9bcc9d0859f8168152bf5c56575441795f
galathinius <anisoara.ionela@gmail.com>
2021-04-23 19:37:39 +0300

    add album page (with post request)

---
+++ flutter_app/lib/views/album/album_request.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import './album_response.dart';
+
+Future<Album> createAlbum(String title) async {
+  final response = await http.post(
+    Uri.https('jsonplaceholder.typicode.com', 'albums'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to create album.');
+  }
+}
---
+++ flutter_app/lib/views/album/album_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import './album_response.dart';
+import './album_request.dart';
+
+class AlbumView extends StatefulWidget {
+  @override
+  AlbumViewState createState() => AlbumViewState();
+}
+
+class AlbumViewState extends State<AlbumView> {
+  final TextEditingController _controller = TextEditingController();
+  Future<Album> _futureAlbum;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Data Example'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(8.0),
+        child: (_futureAlbum == null)
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(hintText: 'Enter Title'),
+                  ),
+                  ElevatedButton(
+                    child: Text('Create Data'),
+                    onPressed: () {
+                      setState(() {
+                        _futureAlbum = createAlbum(_controller.text);
+                      });
+                    },
+                  ),
+                ],
+              )
+            : FutureBuilder<Album>(
+                future: _futureAlbum,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data.title);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+
+                  return CircularProgressIndicator();
+                },
+              ),
+      ),
+    );
+  }
+}
